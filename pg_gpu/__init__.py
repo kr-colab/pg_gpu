60fb20019d380c5b6371ccdfdfd00f915caff074
Andrew Kern <adkern@uoregon.edu>
2025-06-16 13:27:13 -0700

    feat: adding further testing

--- pg_gpu/__init__.py
+++ pg_gpu/__init__.py
@@ -1,11 +1,12 @@
 # pg_gpu - GPU-accelerated population genetics statistics
 
 from . import ld_statistics
-from . import divergence
 from . import diversity
+from . import divergence
+from . import windowed_analysis
 from .haplotype_matrix import HaplotypeMatrix
 from .windowed_analysis import WindowedAnalyzer, windowed_analysis
 
-__all__ = ['ld_statistics', 'divergence', 'diversity', 'HaplotypeMatrix', 'WindowedAnalyzer', 'windowed_analysis']
+__all__ = ['ld_statistics', 'diversity', 'divergence', 'windowed_analysis', 'HaplotypeMatrix', 'WindowedAnalyzer', 'windowed_analysis']
 
 __version__ = '0.1.0'
